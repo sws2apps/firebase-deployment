dd8375009565cbcac16f4222a832e8694c04b50f
Rasamoelina, Haja Onjatiana <26148770+rhahao@users.noreply.github.com>
2022-06-11 13:43:19 +0300

    fix(actions): restore using input

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 A GitHub Action to deploy firebase hosting and functions together.
 
-- Make sure you have the `firebase.json` file in the repository
+- Make sure you have the `firebase.json` file in the root repository, or set a custom path to look for it using the `config` input.
+- Set the project name in the `project` input.
 - Get the Firebase token by running `firebase login:ci` and [store it](https://help.github.com/en/articles/virtual-environments-for-github-actions#creating-and-using-secrets-encrypted-variables) as the `FIREBASE_TOKEN` secret
-- Set the project name in the `FIREBASE_PROJECT` secret
 
 ## Example Usage
 
@@ -13,15 +13,17 @@ name: Deploy to Firebase
 on:
   push:
     branches:
-      - master
+      - main
 jobs:
   main:
     name: Build and Deploy
     runs-on: ubuntu-latest
     steps:
       - name: Deploy to Firebase
-        uses: sws2apps/firebase-deployment@[--pin--]
+        uses: sws2apps/firebase-deployment@e6dfe2cb17dbfeecf7910f5c1d4fee2f0c07deb5
+        with:
+          project: 'my-firebase-project' #required
+          config: 'frontend/firebase.json' #optional
         env:
           FIREBASE_TOKEN: ${{ secrets.FIREBASE_TOKEN }}
-          FIREBASE_PROJECT: ${{ secrets.FIREBASE_PROJECT }}
 ```
--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
+// dependencies
 import core from '@actions/core';
 import exec from '@actions/exec';
 
-const options = {};
-options.cwd = process.env.GITHUB_WORKSPACE;
+const run = async () => {
+	try {
+		// preflight check before starting the actions
+		const project = core.getInput('project');
+		if (!project) {
+			core.setFailed('The Firebase project is missing from the worflow file');
+			return;
+		}
 
-await exec.exec('npm i -g firebase-tools', [], options);
+		if (!process.env.FIREBASE_TOKEN) {
+			core.setFailed('The FIREBASE_TOKEN is missing');
+			return;
+		}
 
-if (!process.env.FIREBASE_TOKEN) {
-	core.setFailed('Oh... The FIREBASE_TOKEN is missing');
-}
+		// get if we receive a custom path for firebase.json
+		const config = core.getInput('config');
 
-if (!process.env.FIREBASE_PROJECT) {
-	core.setFailed('Oh... The FIREBASE_PROJECT is missing');
-}
+		// installing firebase tools
+		await exec.exec('npm i -g firebase-tools');
 
-try {
-	await exec.exec(
-		`firebase deploy -m ${process.env.GITHUB_SHA} --project ${process.env.FIREBASE_PROJECT}`,
-		[],
-		options
-	);
-} catch (error) {
-	core.setFailed(
-		`Oh... An error occured while deploying to Firebase: ${error}`
-	);
-}
+		// attempt to run firebase deploy, and throw an error if failed
+
+		await exec.exec(
+			`firebase deploy -m ${process.env.GITHUB_SHA} ${
+				config ? `--config ${config}` : ''
+			} --project ${project}`
+		);
+	} catch (error) {
+		core.setFailed(`An error occured while deploying to Firebase: ${error}`);
+	}
+};
+
+run();
