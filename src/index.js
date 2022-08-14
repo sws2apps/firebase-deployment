5ec7cb0b04aa2d99366123edbc1510bac441dc73
rhahao <rhahao.vj@gmail.com>
2022-08-14 22:59:51 +0300

    testing new auth

--- src/index.js
+++ src/index.js
@@ -3,29 +3,29 @@ import core from '@actions/core';
 import exec from '@actions/exec';
 
 const run = async () => {
+	// preflight check before starting the actions
+	const project = core.getInput('project');
+	if (!project) {
+		core.setFailed('The Firebase project is missing from the worflow file');
+		return;
+	}
+
+	if (!process.env.GOOGLE_APPLICATION_CREDENTIALS) {
+		core.setFailed('The GOOGLE_APPLICATION_CREDENTIALS is missing');
+		return;
+	}
+
+	// check if we receive a custom path for firebase.json
+	const config = core.getInput('config');
+
+	// check only deployment settings
+	let deployOnly = core.getInput('function') === 'true' ? 'function' : '';
+	deployOnly +=
+		core.getInput('hosting') === 'true'
+			? `${deployOnly !== '' ? ' ' : ''}hosting`
+			: '';
+
 	try {
-		// preflight check before starting the actions
-		const project = core.getInput('project');
-		if (!project) {
-			core.setFailed('The Firebase project is missing from the worflow file');
-			return;
-		}
-
-		if (!process.env.FIREBASE_TOKEN) {
-			core.setFailed('The FIREBASE_TOKEN is missing');
-			return;
-		}
-
-		// check if we receive a custom path for firebase.json
-		const config = core.getInput('config');
-
-		// check only deployment settings
-		let deployOnly = core.getInput('function') === 'true' ? 'function' : '';
-		deployOnly +=
-			core.getInput('hosting') === 'true'
-				? `${deployOnly !== '' ? ' ' : ''}hosting`
-				: '';
-
 		// installing firebase tools
 		await exec.exec('npm i -g firebase-tools');
 
